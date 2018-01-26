e9db4c39d6e46f355d3579bbf19771f2b08dfda4
Lillian Chernin <l.smith1654@gmail.com>
2018-01-25 22:04:36 -0600

    add boilerplate to level html files

--- main.js
+++ main.js
@@ -1,7 +1,15 @@
-const pageData = [
-  
-]
+console.log('javascript is linked!');
 
-$('#nextStep').on('click', () => {
-
-})
+// const pageData = [
+//   {
+//     storyHeader: "Choose your breakfast",
+//     storyParagraph: "Good morning, young fighter.  Fructar has been spotted just beyond the forest! Before you go, fuel up with some breakfast",
+//
+//   },
+//
+//
+// ]
+//
+// $('#nextStep').on('click', () => {
+//
+// })
